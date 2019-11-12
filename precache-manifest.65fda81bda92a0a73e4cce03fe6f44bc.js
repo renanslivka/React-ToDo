670763e562d41cef6c5dcbd3d69b20832b204efc
renanslivka <renanslivka@gmail.com>
2019-11-13 00:01:56 +0200

    Updates

---
+++ precache-manifest.65fda81bda92a0a73e4cce03fe6f44bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19231a3b7d5d0475adde71b8437f8846",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "e13884e88cf69ca1c37c",
+    "url": "/React-ToDo/static/css/main.158ac839.chunk.css"
+  },
+  {
+    "revision": "764d239d41a03428d15a",
+    "url": "/React-ToDo/static/js/2.cc72c28d.chunk.js"
+  },
+  {
+    "revision": "e13884e88cf69ca1c37c",
+    "url": "/React-ToDo/static/js/main.bc4af80e.chunk.js"
+  },
+  {
+    "revision": "a5dc0a80f173be3ce1d8",
+    "url": "/React-ToDo/static/js/runtime-main.9fe437a6.js"
+  }
+]);
